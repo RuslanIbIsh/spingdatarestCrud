0fd94dc461fb357260aa148875237f5db5f70d01
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-11-11 15:36:18 +0200

    Initial commit

---
+++ src/test/java/com/iri/spingdatarestCrud/SpingdatarestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iri.spingdatarestCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingdatarestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
